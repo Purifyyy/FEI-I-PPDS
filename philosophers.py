87780957c25a15776a542ccf76e0066004bce780
Purifyyy <purifyyy@gmail.com>
2023-03-12 15:30:05 +0100

    docs: formulate docstrings

--- philosophers.py
+++ philosophers.py
@@ -1,6 +1,9 @@
 """
 Dinning philosophers problem simulation.
 
+This module defines a solution to the Dining Philosophers problem,
+which involves five philosophers sitting around a table, alternating
+between thinking and eating, and using forks.
 """
 
 
@@ -15,21 +18,51 @@ NUM_RUNS: int = 10  # number of repetitions of think-eat cycle of philosophers
 
 
 class Shared:
+    """
+    This class that represents the shared resources among the philosophers, i.e., the forks.
+
+    Attributes:
+        forks (list) -- a list of Mutex objects, one for each fork on the table
+    """
     def __init__(self):
         self.forks = [Mutex() for _ in range(NUM_PHILOSOPHERS)]
 
 
 def think(i: int):
+    """
+    Simulates a philosopher thinking.
+
+    Arguments:
+        i (int) -- the identifier of the philosopher
+    """
     print(f"Philosopher {i} is thinking!")
     sleep(0.1)
 
 
 def eat(i: int):
+    """
+    Simulates a philosopher eating a meal.
+
+    Arguments:
+        i (int) -- the identifier of the philosopher
+    """
     print(f"Philosopher {i} is eating!")
     sleep(0.1)
 
 
 def philosopher(i: int, shared: Shared):
+    """
+    Simulates a process of thinking, eating of a philosopher.
+
+    A philosopher thinks, when he gets hungry, he attempts to raise forks on both of his sides.
+    If both forks are not available, the philosopher will wait until they become available.
+    Once the philosopher has finished eating, they will put down both of the forks and return
+    to a state of thinking.
+
+    Arguments:
+        i (int) -- the identifier of the philosopher
+        shared (Shared) -- an instance of the Shared class containing a list of fork mutexes
+    """
     for _ in range(NUM_RUNS):
         think(i)
 
