70bec96c46d8e346e16d51ac7e140dcc405b8dbe
Purifyyy@laptop <xbadurat@stuba.sk>
2023-03-14 20:36:51 +0100

    feat: implement cook, move savage inside barrier

--- savages.py
+++ savages.py
@@ -8,20 +8,21 @@ Dinning savages simulation.
 __authors__ = "Tomáš Baďura"
 
 
-from fei.ppds import Thread, Mutex, Semaphore, print
+from fei.ppds import Thread, Mutex, Semaphore, print, Event
 from time import sleep
 
-H: int = 5  # number of portions in a full pot
+H: int = 10  # number of portions in a full pot
 D: int = 3  # number of savages
-K: int = 2  # number of cooks
+K: int = 4  # number of cooks
 
 
 class Shared:
     def __init__(self):
         self.pot = H
-        self.mutex = Mutex()
+        self.savage_mutex = Mutex()
+        self.cook_mutex = Mutex()
         self.full_pot = Semaphore(0)
-        self.empty_pot = Semaphore(K)
+        self.empty_pot = Event()
 
         self.barrier_count = 0
         self.barrier_mutex = Mutex()
@@ -30,7 +31,8 @@ class Shared:
 
 
 def put_portion(i: int, shared: Shared):
-    print(f"put_portion {i}")
+    shared.pot += 1
+    print(f"Cook {i}: [{shared.pot}/{H}]")
 
 
 def get_portion(i: int, shared: Shared):
@@ -39,7 +41,16 @@ def get_portion(i: int, shared: Shared):
 
 
 def cook(i: int, shared: Shared):
-    print(f"cook {i}")
+    while True:
+        shared.empty_pot.wait()
+        shared.cook_mutex.lock()
+        shared.empty_pot.wait()
+        put_portion(i, shared)
+        if shared.pot == H:
+            shared.empty_pot.clear()
+            shared.full_pot.signal()
+            shared.cook_mutex.unlock()
+        shared.cook_mutex.unlock()
 
 
 def savage(i: int, shared: Shared):
@@ -52,7 +63,15 @@ def savage(i: int, shared: Shared):
         shared.barrier_mutex.unlock()
         shared.turnstile1.wait()
 
-        # everyone ready, set, feast
+        shared.savage_mutex.lock()
+        if shared.pot == 0:
+            print(f"Savage [{i}]: There is no food left, I'm waking up the chefs")
+            shared.empty_pot.signal()
+            shared.full_pot.wait()
+        get_portion(i, shared)
+        shared.savage_mutex.unlock()
+
+        sleep(0.2)  # pseudo-eating state
 
         shared.barrier_mutex.lock()
         shared.barrier_count -= 1
@@ -60,16 +79,6 @@ def savage(i: int, shared: Shared):
             shared.turnstile2.signal(D)
         shared.barrier_mutex.unlock()
         shared.turnstile2.wait()
-
-        shared.mutex.lock()
-        if shared.pot == 0:
-            print(f"Savage [{i}]: There is no food left, I'm waking up the chefs")
-            shared.empty_pot.signal(K)
-            shared.full_pot.wait()
-        get_portion(i, shared)
-        shared.mutex.unlock()
-
-        sleep(0.2)  # pseudo-eating state
 
 
 def main():
