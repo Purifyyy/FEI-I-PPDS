9608dcefb7fdf7f42b53c6025cc5c1db0c295631
Purifyyy@laptop <xbadurat@stuba.sk>
2023-03-15 17:53:07 +0100

    docs: add docstrings, module header, comments

--- savages.py
+++ savages.py
@@ -1,7 +1,10 @@
 """
+Sophisticated savages feast simulation.
 
-Dinning savages simulation.
-
+A simulation where a group of "savages" share a communal pot of food.
+Each savage takes one serving from the pot, and when it's empty,
+cooks refill it. The cooks can only refill the pot one portion by one
+when all the savages are not currently eating.
 """
 
 
@@ -17,13 +20,26 @@ K: int = 4  # number of cooks
 
 
 class Shared:
+    """
+    This class that represents the shared resources among the savages and cooks.
+
+    Attributes:
+        pot (int) -- the number of portions remaining in the pot of food
+        savage_mutex (Mutex) -- a mutex lock to provide mutual exclusion between savages
+        cook_mutex (Mutex) -- a mutex lock providing mutual exclusion between cooks
+        full_pot (Semaphore) -- a semaphore to signal the savages that the pot has been refilled
+        empty_pot (Event) -- an event that the cooks wait for, and the savages signal, when in need of pot refill
+        barrier_count (int) -- savages count, when going through the tourniquet
+        barrier_mutex (Mutex) -- a lock that ensures mutual exclusion when going through tourniquet
+        turnstile1 (Semaphore) -- a semaphore that serves as an entrance tourniquet
+        turnstile2 (Semaphore) -- semaphore serving as an exit tourniquet
+    """
     def __init__(self):
         self.pot = H
         self.savage_mutex = Mutex()
         self.cook_mutex = Mutex()
         self.full_pot = Semaphore(0)
         self.empty_pot = Event()
-
         self.barrier_count = 0
         self.barrier_mutex = Mutex()
         self.turnstile1 = Semaphore(0)
@@ -31,25 +47,50 @@ class Shared:
 
 
 def put_portion(i: int, shared: Shared):
+    """
+    Simulates a cook adding a cooked portion to a shared food pot.
+
+    Arguments:
+        i (int) -- the identifier of the cook
+        shared (Shared) -- an instance of the Shared class containing shared resources and variables
+    """
     shared.pot += 1
     print(f"\033[34mCook [{i}]: I've added a portion. [{shared.pot}/{H}]\033[00m")
 
 
 def get_portion(i: int, shared: Shared):
+    """
+    Simulates a savage taking a portion out of the shared food pot.
+
+    Arguments:
+        i (int) -- the identifier of the savage
+        shared (Shared) -- an instance of the Shared class containing shared resources and variables
+    """
     shared.pot -= 1
     print(f"Savage [{i}]: I've took a portion and I'm going to eat now. [{shared.pot}/{H} left]")
 
 
 def cook(i: int, shared: Shared):
+    """
+    Simulates a cook preparing and adding a food portion to the food pot.
+
+    Cook prepares a food portion and waits until the pot gets empty,
+    then checks whether the pot if fully refilled, if not he adds his
+    portion, if yes he signals other cooks, and they wait again.
+
+    Arguments:
+        i (int) -- the identifier of the cook
+        shared (Shared) -- an instance of the Shared class containing shared resources and variables
+    """
     while True:
 
         sleep(0.3)  # cooking a portion
 
         shared.cook_mutex.lock()
-        shared.empty_pot.wait()
+        shared.empty_pot.wait()  # wait for the savages to empty the pot
         if shared.pot == H:
-            shared.empty_pot.clear()
-            shared.full_pot.signal()
+            shared.empty_pot.clear()  # reset event to waiting state for the cooks
+            shared.full_pot.signal()  # signal savages to continue feasting
             shared.cook_mutex.unlock()
             print(f"\033[92mCook [{i}]: The pot is full, savages may continue.\033[00m")
             continue
@@ -58,6 +99,19 @@ def cook(i: int, shared: Shared):
 
 
 def savage(i: int, shared: Shared):
+    """
+    Simulates a savage participating in the feast.
+
+    Savage waits until all savages are present, once that occurs
+    he takes a portion of the food from the pot and starts eating.
+    If there is no food left in the pot, he signals the cooks to
+    refill it. Once all the savages finish eating, this process
+    repeats.
+
+    Arguments:
+        i (int) -- the identifier of the cook
+        shared (Shared) -- an instance of the Shared class containing shared resources and variables
+    """
     while True:
 
         shared.barrier_mutex.lock()
@@ -72,7 +126,7 @@ def savage(i: int, shared: Shared):
         if shared.pot == 0:
             print(f"\033[31mSavage [{i}]: There is no food left, I'm waking up the cooks.\033[00m")
             shared.empty_pot.signal()
-            shared.full_pot.wait()
+            shared.full_pot.wait()  # wait for the cooks to refill the pot
         get_portion(i, shared)
         shared.savage_mutex.unlock()
 
